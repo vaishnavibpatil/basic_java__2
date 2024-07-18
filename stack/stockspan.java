c8e95619aa237322af69f23498adfd14b96c4088
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-07-18 11:40:00 +0530

    Add files via upload

---
+++ stack/stockspan.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class stockspan 
+{
+    public static void checkspan( int span[],int stock[])
+    {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+
+        for(int i=1;i<stock.length;i++)
+        {
+            int currprice = stock[i];
+            while(!s.isEmpty() && currprice > stock[s.peek()])
+            {
+                s.pop();
+            }
+            if(s.isEmpty())
+            {
+                span[i]= i + 1;
+            }
+            else
+            {
+                int prevhigh = s.peek();
+                span[i] = i - prevhigh;
+            }
+            s.push(i);;
+        }
+
+    }
+    public static void main(String args[])
+    {
+        
+        int stock[] = {100,80,60,70,60,85,100};
+        int span[] = new int[stock.length];
+
+        checkspan(span,stock);
+        for(int i=0;i<span.length;i++)
+        {
+            System.out.println(span[i]);
+        }
+
+    }
+    
+}
