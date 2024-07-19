93be8087e37ac34e56e748bcf4e28edb1319311a
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-07-20 00:26:35 +0530

    Add files via upload

--- stack_2/valid_parenthesis.java
+++ stack_2/valid_parenthesis.java
@@ -1,19 +1,47 @@
 import java.util.*;
 public class valid_parenthesis
 {
-    public static void valid(String str)
+    public static boolean valid(String str)
     {
         Stack<Character> s = new Stack<>();
         for(int i=0 ; i<str.length() ;i++)
         {
             char ch = str.charAt(i);
             if(ch == '{' || ch == '[' || ch == '(')
-
+            {
+                s.push(ch);
+            }
+            else
+            {
+                if(s.isEmpty())
+                {
+                  return false;
+                }
+                else if((s.peek() =='{' && ch =='}' ) 
+                ||(s.peek()=='(' && ch ==')') 
+                || (s.peek()=='[' && ch==']'))
+                {
+                    s.pop();
+                }
+                else
+                {
+                    return false;
+                }
+  
+           }
+        }
+        if(s.isEmpty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
     public static void main(String args[])
     {
-        String str = "{[()]}";
-        valid(str);
+        String str = "{[()]}()";
+        System.out.print(valid(str));
     }
 }
