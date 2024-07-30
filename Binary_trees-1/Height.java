25970197bccc6e39cace8736f69d32a8e63afe2f
vaishnavibpatil <116648404+vaishnavibpatil@users.noreply.github.com>
2024-07-30 13:16:33 +0530

    Add files via upload

---
+++ Binary_trees-1/Height.java
@@ -0,0 +1,47 @@
+public class Height
+{
+    static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data)
+        {
+            this.data = data;
+            left= null;
+            right =null;
+        }
+    }
+    public static int Hei(Node root)
+        { 
+            if(root == null)
+            {
+                return 0;
+            }
+             int lh = Hei(root.left);
+             int rh = Hei(root.right);
+             return Math.max(lh,rh)+1;
+            
+        }
+    
+    public static void main(String args[])
+    {
+        
+
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.print("height of the tree = " + Hei(root));
+
+
+        
+    }
+    
+}
